18aa586e51cbd9a04873854c33ca952e563ec505
Nathaniel <nathaniel.wallace@gmail.com>
2023-10-12 19:10:34 -0400

    initial setup querying working assign unique ids to all components

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,31 @@
-export default function Home() {
-  return <div>Next JS &amp; WordPress course.</div>;
+import { gql } from "@apollo/client";
+import client from "../client";
+import { BlockRenderer } from "components/blockRenderer";
+import { cleanAndTransformBlocks } from "utils/cleanAndTransformBlocks";
+
+export default function Home(props) {
+  console.log(props)
+  return <div><BlockRenderer blocks={props.blocks} /></div>;
 }
+
+export const getStaticProps = async () => {
+  const { data } = await client.query({
+    query: gql`
+      query NewQuery {
+  nodeByUri(uri: "/") {
+    ... on Page {
+      id
+      blocks
+    }
+  }
+}
+    `,
+  });
+  
+  return {
+    props: {
+      blocks: cleanAndTransformBlocks(data.nodeByUri.blocks),
+      
+    },
+  };
+};
