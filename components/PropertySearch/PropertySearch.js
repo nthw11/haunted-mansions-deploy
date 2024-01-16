6ef9ed0254c4debe1e271789b1f8fcf14576f4df
Nathaniel <nathaniel.wallace@gmail.com>
2024-01-16 18:01:32 -0500

    search and pagination working

--- components/PropertySearch/PropertySearch.js
+++ components/PropertySearch/PropertySearch.js
@@ -1,23 +1,89 @@
 import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import queryString from "query-string";
 import { Results } from "./Results";
+import { Pagination } from "./Pagination";
+import { Filters } from "./Filters";
 
 export const PropertySearch = () => {
-  const [ properties, setProperties ] = useState([]);
+  const router = useRouter();
+  const [properties, setProperties] = useState([]);
+  const [totalResults, setTotalResults] = useState(0);
+  const pageSize = 3;
+
+  const search = async () => {
+    const { page, minPrice, maxPrice, hasParking, petFriendly } =
+      queryString.parse(window.location.search);
+    const filters = {};
+    if (minPrice) {
+      filters.minPrice = parseInt(minPrice);
+    }
+    if (maxPrice) {
+      filters.maxPrice = parseInt(maxPrice);
+    }
+    if (hasParking === "true") {
+      filters.hasParking = true;
+    }
+    if (petFriendly === "true") {
+      filters.petFriendly = true;
+    }
+
+    const response = await fetch("/api/search", {
+      method: "POST",
+      body: JSON.stringify({
+        page: parseInt(page) || 1,
+        ...filters,
+      }),
+    });
+    const data = await response.json();
+    setProperties(data.properties);
+    setTotalResults(data.total);
+  };
+
+  const handlePageClick = async (pageNumber) => {
+    const {
+      petFriendly,
+      hasParking,
+      minPrice,
+      maxPrice
+    } = queryString.parse(window.location.search);
+    await router.push(
+      `
+      ${router.query.slug.join("/")}?page=${pageNumber}&petFriendly=${petFriendly === "true"}&hasParking=${hasParking === "true"}&minPrice=${minPrice}&maxPrice=${maxPrice}`,
+      null,
+      { shallow: true }
+    );
+  };
+  search();
   useEffect(() => {
-    console.log("PropertySearch mounted");
-    const search = async () => {
-      const response = await fetch("/api/search");
-      const data = await response.json();
-      console.log('search data: ', data);
-      setProperties(data.properties);
-      
-    };
     search();
-    return () => console.log("PropertySearch unmounted");
   }, []);
+
+  const handleSearch = async ({
+    petFriendly,
+    hasParking,
+    minPrice,
+    maxPrice,
+  }) => {
+    await router.push(
+      `
+      ${router.query.slug.join(
+        "/"
+      )}?page=1&petFriendly=${!!petFriendly}&hasParking=${!!hasParking}&minPrice=${minPrice}&maxPrice=${maxPrice}`,
+      null,
+      { shallow: true }
+    );
+    search();
+  };
+
   return (
     <div className="property-search">
+      <Filters onSearch={handleSearch} />
       <Results properties={properties} />
+      <Pagination
+        totalPages={Math.ceil(totalResults / pageSize)}
+        onPageClick={handlePageClick}
+      />
     </div>
   );
 };
--- pages/api/search.js
+++ pages/api/search.js
@@ -3,10 +3,64 @@ import { gql } from "@apollo/client";
 
 const handler = async (req, res) => {
   try {
+    const filters = JSON.parse(req.body);
+
+    let hasParkingFilter = ``
+    let petFriendlyFilter = ``
+    let minPriceFilter = ``
+    let maxPriceFilter = ``
+
+    if (filters.hasParking) {
+      hasParkingFilter = `{
+        key: "has_parking"
+        compare: EQUAL_TO
+        value: "1"
+      },`
+    }
+    if (filters.petFriendly) {
+      petFriendlyFilter = `{
+        key: "pet_friendly"
+        compare: EQUAL_TO
+        value: "1"
+      },`
+    }
+    if (filters.minPrice){
+      minPriceFilter = `{
+        key: "price"
+        compare: GREATER_THAN_OR_EQUAL_TO
+        value: "${filters.minPrice}"
+        type: NUMERIC
+      },`
+    }
+    if (filters.maxPrice){
+      maxPriceFilter = `{
+        key: "price"
+        compare: LESS_THAN_OR_EQUAL_TO
+        value: "${filters.maxPrice}"
+        type: NUMERIC
+      },`
+    }
     const { data } = await client.query({
       query: gql`
         query AllPropertiesQuery {
-          properties {
+          properties(where: { offsetPagination: { size: 3, offset: ${
+            ((filters.page || 1) - 1) * 3
+          } }
+          metaQuery: {
+            relation: AND
+            metaArray: [
+              ${petFriendlyFilter}
+              ${hasParkingFilter}
+              ${minPriceFilter}
+              ${maxPriceFilter}
+            ]
+          }
+           } ) {
+            pageInfo {
+              offsetPagination {
+                total
+              }
+            }
             nodes {
               title
               uri
@@ -29,7 +83,10 @@ const handler = async (req, res) => {
         }
       `,
     });
-    res.status(200).json({ properties: data.properties.nodes });
+    res.status(200).json({
+      total: data.properties.pageInfo.offsetPagination.total,
+      properties: data.properties.nodes,
+    });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Something went wrong!" });
@@ -37,3 +94,4 @@ const handler = async (req, res) => {
 };
 
 export default handler;
+
